694d9a538a8a932d4fdc2d52af3787bda2618e29
Poppaswoodcreations <poppas.wooden.creations@gmail.com>
2025-12-14 10:56:43 +1300

    Update blogData.ts

--- src/pages/blog/blogData.ts
+++ src/pages/blog/blogData.ts
@@ -14,6 +14,10 @@ export interface BlogPost {
   featuredImage: string;
   imageAlt: string;
   content: React.ReactNode;
+  faqs?: Array<{
+    question: string;
+    answer: string;
+  }>;
 }
 
 export const blogPosts: BlogPost[] = [
