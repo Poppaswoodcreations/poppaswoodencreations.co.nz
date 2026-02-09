8f0fe1a126970f7b6b69e622595de35eb3943349
Poppaswoodcreations <poppas.wooden.creations@gmail.com>
2026-02-10 08:46:06 +1300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import Products from './pages/Products';
-import ProductDetail from './pages/ProductDetail';
-import Cart from './pages/Cart';
-import Checkout from './pages/Checkout';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Profile from './pages/Profile';
-import OrderHistory from './pages/OrderHistory';
-import Blog from './pages/Blog';
-import BlogPost from './pages/BlogPost';
-import AdminDashboard from './pages/admin/Dashboard';
-import AdminProducts from './pages/admin/Products';
-import AdminOrders from './pages/admin/Orders';
-import AdminBlog from './pages/admin/Blog';
-import AdminReviews from './pages/admin/Reviews';
-import PrivateRoute from './components/PrivateRoute';
-import AdminRoute from './components/AdminRoute';
+import HomePage from './pages/HomePage';
+import ProductsPage from './pages/ProductsPage';
+import ProductDetailPage from './pages/ProductDetailPage';
+import BlogPage from './pages/BlogPage';
+import BlogPostPage from './pages/BlogPostPage';
+import CartPage from './pages/CartPage';
+import CheckoutPage from './pages/CheckoutPage';
+import ReviewsPage from './pages/ReviewsPage';
 
 // Track pageviews on route changes
 function usePageTracking() {
@@ -34,71 +23,20 @@ function usePageTracking() {
   }, [location]);
 }
 
-// App content - must be inside Router to use useLocation
 function AppContent() {
   usePageTracking();
   
   return (
     <HelmetProvider>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          {/* Public routes */}
-          <Route index element={<Home />} />
-          <Route path="products" element={<Products />} />
-          <Route path="products/:slug" element={<ProductDetail />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path="blog" element={<Blog />} />
-          <Route path="blog/:slug" element={<BlogPost />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-
-          {/* Protected routes */}
-          <Route path="checkout" element={
-            <PrivateRoute>
-              <Checkout />
-            </PrivateRoute>
-          } />
-          <Route path="profile" element={
-            <PrivateRoute>
-              <Profile />
-            </PrivateRoute>
-          } />
-          <Route path="orders" element={
-            <PrivateRoute>
-              <OrderHistory />
-            </PrivateRoute>
-          } />
-
-          {/* Admin routes */}
-          <Route path="admin" element={
-            <AdminRoute>
-              <AdminDashboard />
-            </AdminRoute>
-          } />
-          <Route path="admin/products" element={
-            <AdminRoute>
-              <AdminProducts />
-            </AdminRoute>
-          } />
-          <Route path="admin/orders" element={
-            <AdminRoute>
-              <AdminOrders />
-            </AdminRoute>
-          } />
-          <Route path="admin/blog" element={
-            <AdminRoute>
-              <AdminBlog />
-            </AdminRoute>
-          } />
-          <Route path="admin/reviews" element={
-            <AdminRoute>
-              <AdminReviews />
-            </AdminRoute>
-          } />
-
-          {/* Catch all - redirect to home */}
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Route>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products/:slug" element={<ProductDetailPage />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/blog/:slug" element={<BlogPostPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/reviews" element={<ReviewsPage />} />
       </Routes>
     </HelmetProvider>
   );
