efb2eee303291c1903710984dbf86eac3915c363
Poppaswoodcreations <poppas.wooden.creations@gmail.com>
2026-05-08 12:45:37 +1200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -184,8 +184,8 @@ const Footer: React.FC = () => {
               </div>
               <div className="flex items-center space-x-3">
                 <Phone className="w-5 h-5 text-amber-500 flex-shrink-0" />
-                <a href="tel:+64210228166" className="text-sm hover:text-amber-500 transition-colors">
-                  +64 21 022 8166 (021 022 8166)
+                <a href="tel:+642102288166" className="text-sm hover:text-amber-500 transition-colors">
+                  +64 21 022 88166 (021 022 88166)
                 </a>
               </div>
               <div className="flex items-center space-x-3">
